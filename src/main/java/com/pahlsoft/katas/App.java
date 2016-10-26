d48560d96a011a1b6c159faec4f7923295840e82
AJ Pahl <ajpahl1008@gmail.com>
2016-10-26 01:32:53 -0400

    First Commit

---
+++ src/main/java/com/pahlsoft/katas/App.java
@@ -0,0 +1,13 @@
+package com.pahlsoft.katas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
